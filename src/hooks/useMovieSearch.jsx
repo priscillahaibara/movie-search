634600e2fdf1c407f3f1ca1fd8c6e75f6926cd10
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-02 20:29:35 -0300

    feat: add debounce to movie search queries

--- src/hooks/useMovieSearch.jsx
+++ src/hooks/useMovieSearch.jsx
@@ -1,5 +1,5 @@
 import useFetch from "./useFetch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 /* 
 Custom hook for movie searching (OMDb API)
@@ -8,9 +8,20 @@ Custom hook for movie searching (OMDb API)
 
 function useMovieSearch(apiKey) {
   const [query, setQuery] = useState("");
-  const url = query
-    ? `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [query]);
+
+  const url = debouncedQuery
+    ? `https://www.omdbapi.com/?apikey=${apiKey}&s=${debouncedQuery}`
     : "";
+
   const { data, error, isLoading } = useFetch(url);
 
   return { data, error, isLoading, query, setQuery };
