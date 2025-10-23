39a177c4cb5652cbeeabeb19087bf81f1c478b49
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-23 19:39:03 -0300

    feat(MovieHeader): implement back navigation using useNavigate

--- src/components/MovieHeader.jsx
+++ src/components/MovieHeader.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./MovieHeader.module.css";
 
 function MovieHeader({ data }) {
+  const navigate = useNavigate();
+  const handleReturn = () => {
+    navigate(-1)
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.title}>
-        <ion-icon name="arrow-undo-outline" className={styles.icon}></ion-icon>
+        <button type="button" onClick={handleReturn}>
+          <ion-icon
+            name="arrow-undo-outline"
+            className={styles.icon}
+          ></ion-icon>
+        </button>
         <h2>
           {data.Title} <span>({data.Year})</span>
         </h2>
