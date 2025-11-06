9df00b17eb819fda60df157a5f35b1077cdcdc79
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-11-05 21:07:17 -0300

    feat: show genres with submenu on lateral menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import styles from "./Menu.module.css";
 
 function Menu({ isOpen, toggleMenu }) {
+  const [showGenres, setShowGenres] = useState(false);
+  const genres = ["Action", "Comedy", "Drama", "Horror", "Romance", "Series"];
+
+  function handleGenres() {
+    setShowGenres((g) => !g);
+  }
+
   return (
     <>
       <div
-        className={`${styles.overlay} ${isOpen ? styles.show : ''}`}
+        className={`${styles.overlay} ${isOpen ? styles.show : ""}`}
         onClick={toggleMenu}
       ></div>
 
-      <ul className={`${styles.menu} ${isOpen ? styles.show : ''}`}>
+      <ul className={`${styles.menu} ${isOpen ? styles.show : ""}`}>
         <button onClick={toggleMenu} className={styles.closeBtn}>
-          <ion-icon name="close-outline"></ion-icon>
+          <ion-icon name="close"></ion-icon>
         </button>
+
+        <li onClick={handleGenres}>
+          <span className={styles.genres}>Genres</span>
+          <ion-icon
+            name={showGenres ? "chevron-up-outline" : "chevron-down-outline"}
+            className={`${styles.chevronIcon} ${showGenres ? styles.rotate : ''}`}
+          ></ion-icon>
+        </li>
+
+        {showGenres && (
+          <ul className={styles.genresList}>
+            {genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        )}
+
         <li>Favorites</li>
         <li>Settings</li>
       </ul>
