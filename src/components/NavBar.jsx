a8855d0a367a999095da341d0e8a6832e17acd09
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-11-06 13:25:29 -0300

    style: update navbar colors for dark mode

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,18 +15,20 @@ function NavBar() {
   return (
     <>
       <nav className={styles.nav}>
-        {/* <button onClick={toggleTheme}>
-        <ion-icon
-          name={theme === "light" ? "moon-outline" : "sunny-outline"}
-        ></ion-icon>
-      </button> */}
-
         <button onClick={toggleMenu}>
-          <ion-icon name={showMenu ? "close" : "menu"}></ion-icon>
+          <ion-icon name={showMenu ? "close" : "menu"} className={styles.icon}></ion-icon>
         </button>
-        <NavLink to="/" className={styles.logo}>
-          CineDB
-        </NavLink>
+
+        <div>
+          <button onClick={toggleTheme} style={{marginRight: '2rem'}}>
+            <ion-icon
+              name={theme === "light" ? "moon-outline" : "sunny-outline"} className={styles.icon}
+            ></ion-icon>
+          </button>
+          <NavLink to="/" className={styles.logo}>
+            CineDB
+          </NavLink>
+        </div>
       </nav>
 
       <Menu isOpen={showMenu} toggleMenu={toggleMenu} />
