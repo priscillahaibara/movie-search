4abea798286b79c6fc347b7b9d05f7e19f8b5307
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-11-21 16:05:26 -0300

    fix menu bugs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,43 +5,41 @@ import { genreMap, capitalizeFirstLetter } from "../utils/helpers";
 import Logo from "./Logo";
 
 function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isMoviesOpen, setIsMoviesOpen] = useState(false);
-  const [isSeriesOpen, setIsSeriesOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [moviesOpen, setMoviesOpen] = useState(false);
+  const [seriesOpen, setSeriesOpen] = useState(false);
 
-  function closeMenu() {
-    setIsOpen(false);
-  }
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
 
-  function toggleMenu() {
-    setIsOpen((prev) => !prev);
-  }
+  const closeMenu = () => {
+    setMenuOpen(false);
+    setMoviesOpen(false);
+    setSeriesOpen(false);
+  };
 
-  function toggleSubmenu(media) {
-    if (media === "movies") {
-      setIsMoviesOpen((m) => !m);
-    } else if (media === "series") {
-      setIsSeriesOpen((s) => !s);
-    }
-  }
+  const toggleSubmenu = (type) => {
+    if (type === "movies") setMoviesOpen((prev) => !prev);
+    if (type === "series") setSeriesOpen((prev) => !prev);
+  };
 
   return (
     <>
-      {isOpen && <div className={styles.overlay} onClick={closeMenu}></div>}
+      {menuOpen && <div className={styles.overlay} onClick={closeMenu}></div>}
 
-      <nav className={styles.navbar}>
+      <header className={styles.header}>
         <Logo />
 
-        <button onClick={toggleMenu}>
-          <ion-icon
-            name={isOpen ? "close" : "menu"}
-            className={styles.hamburger}
-          ></ion-icon>
+        <button onClick={toggleMenu} className={styles.iconButton}>
+          <ion-icon name="menu" class={styles.hamburger} />
         </button>
 
-        <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+        <ul className={`${styles.menu} ${menuOpen ? styles.open : ""}`}>
+          <button onClick={closeMenu}>
+            <ion-icon name="close" class={styles.close} />
+          </button>
+
           <li>
-            <Link to="/" onClick={toggleMenu}>
+            <Link to="/" onClick={closeMenu}>
               Home
             </Link>
           </li>
@@ -55,11 +53,32 @@ function NavBar() {
             </div>
 
             <ul
-              className={`${styles.submenu} ${isMoviesOpen ? styles.show : ""}`}
+              className={`${styles.submenu} ${moviesOpen ? styles.show : ""}`}
             >
               {Object.keys(genreMap.movie).map((genre) => (
-                <li key={genre}>
-                  <Link to={`/movie/${genre}`} onClick={toggleMenu}>
+                <li key={genre} className={styles.genreItem}>
+                  <Link to={`/movie/${genre}`} onClick={closeMenu}>
+                    {capitalizeFirstLetter(genre)}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+
+          <li
+            className={styles.hasSubmenu}
+            onClick={() => toggleSubmenu("movies")}
+          >
+            <div>
+              <span>Series</span>
+            </div>
+
+            <ul
+              className={`${styles.submenu} ${seriesOpen ? styles.show : ""}`}
+            >
+              {Object.keys(genreMap.tv).map((genre) => (
+                <li key={genre} className={styles.genreItem}>
+                  <Link to={`/tv/${genre}`} onClick={closeMenu}>
                     {capitalizeFirstLetter(genre)}
                   </Link>
                 </li>
@@ -68,52 +87,20 @@ function NavBar() {
           </li>
 
           <li>
-            <Link to="/favorites" onClick={toggleMenu}>
+            <Link to="/favorites" onClick={closeMenu}>
               Favorites
             </Link>
           </li>
 
           <li>
-            <Link to="/settings" onClick={toggleMenu}>
+            <Link to="/settings" onClick={closeMenu}>
               Settings
             </Link>
           </li>
         </ul>
-      </nav>
+      </header>
     </>
   );
 }
 
 export default NavBar;
-
-/* import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import styles from "./NavBar.module.css";
-import Menu from "./Menu";
-
-function NavBar() {
-  const [showMenu, setShowMenu] = useState(false);
-
-  function toggleMenu() {
-    setShowMenu((m) => !m);
-  }
-
-  return (
-    <>
-      <nav className={styles.nav}>
-        <NavLink to="/" className={styles.logo}>
-          CineDB
-        </NavLink>
-
-        <button onClick={toggleMenu}>
-          <ion-icon name="menu" className={styles.hamburger}></ion-icon>
-        </button>
-
-        <Menu isOpen={showMenu} toggleMenu={toggleMenu} />
-      </nav>
-    </>
-  );
-}
-
-export default NavBar;
- */
